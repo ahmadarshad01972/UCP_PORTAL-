8bd429169bcb75fd939f56d687e2e7a8ad3e68d3
ahmadarshad01972 <ahmadarshad01972@gmail.com>
2025-05-25 01:43:03 +0500

    Update scraper.py

--- backend/scraper.py
+++ backend/scraper.py
@@ -45,7 +45,7 @@ def scrape_user(user, i):
     )
 
     options = Options()
-    #options.add_argument("--headless=new")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
